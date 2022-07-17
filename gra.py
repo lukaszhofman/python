8cae1d3010df85842fc9fd77600b69b5fabe8c33
lukaszhofman <109438013+lukaszhofman@users.noreply.github.com>
2022-07-17 21:44:25 +0200

    Add files via upload

---
+++ gra.py
@@ -0,0 +1,255 @@
+import os
+
+def print_loch():
+    print()
+    print("      ___1__             ___2__             ___3__      ")
+    print("   ,-' ;  ! `-.       ,-' ;  ! `-.       ,-' ;  ! `-.    ")
+    print("  / :  !  :  . \     / :  !  :  . \     / :  !  :  . \   ")
+    print(" |_ ;   __:  ;  |   |_ ;   __:  ;  |   |_ ;   __:  ;  | ") 
+    print(" )| .  :)(.  !  |   )| .  :)(.  !  |   )| .  :)(.  !  | ") 
+    print(" |  :  ;`'  (_) (   |  :  ;`'  (_) (   |  :  ;`'  (_) ( ") 
+    print(" |  :  :  .     |   |  :  :  .     |   |  :  :  .     |  ")
+    print(" )_ !  ,  ;  ;  |   )_ !  ,  ;  ;  |   )_ !  ,  ;  ;  | ") 
+    print(" || .  .  :  :  |   || .  .  :  :  |   || .  .  :  :  | ")  
+    print(" |mt-2_;----.___|   |mt-2_;----.___|   |mt-2_;----.___| ") 
+    print()
+
+def print_zamek():
+    print()
+    print("                                               /\      /\ ")
+    print("                                               |______| | ")
+    print("                                               || ^  ^ ||")
+    print("                                               \| |  | |/")
+    print("                                                |______|")
+    print("              __                                |  __  |")
+    print("             /  \       ________________________|_/  \_|__")
+    print("            / ^^ \     /=========================/ ^^ \===|")
+    print("           /  []  \   /=========================/  []  \==|")
+    print("          /________\ /=========================/________\=|")
+    print("       *  |        |/==========================|        |=|")
+    print("      *** | ^^  ^^ |---------------------------| ^^  ^^ |--")
+    print("     *****| []  [] |           _____           | []  [] | |")
+    print("    *******        |          /_____\          |      * | |")
+    print("   *********^^  ^^ |  ^^  ^^  |  |  |  ^^  ^^  |     ***| |")
+    print("  ***********]  [] |  []  []  |  |  |  []  []  | ===***** |")
+    print(" *************     |         @|__|__|@         |/ |*******|")
+    print("***************   ***********--=====--**********| *********")
+    print("***************___*********** |=====| **********|***********")
+    print(" *************     ********* /=======\ ******** | *********")
+
+def print_ork():
+    print()
+    print("                        _,.---''```````'-.")
+    print("                    ,-'`                  `-._")
+    print("                 ,-`                   __,-``,x")
+    print("                /             _       /,'  ,|/ |")
+    print("              ,'         ,''-<_`'.    |  ,' |  |")
+    print("             /          / _    `  `.  | / \ |\  |")
+    print("             |         (  |`'-,---, `'  \_|/ |  |")
+    print("             |         |`  \  \|  /  __,    _ \ |")
+    print("             |         |    `._\,'  '    ,-`_\ \|")
+    print("             |         |        ,----      /|   )")
+    print("             \         \       / --.      {/   /|")
+    print("             \         | |       `.\         / |")
+    print("               \        / `-.                 | /")
+    print("                `.     |     `-        _,--V`)\/        _,-")
+    print("                  `,   |           /``V_,.--`  \.  _,-'`")
+    print("                   /`--'`._        `-'`         )`'")
+    print("                  /        `-.            _,.-'`")
+    print("                              `-.____,.-'`")
+    print()
+
+def print_skrzynia():
+    print()
+    print("                      _.--. ")
+    print("                  _.-'_:-'|| ")
+    print("              _.-'_.-::::'|| ")
+    print("         _.-:'_.-::::::'  || ")
+    print("       .'`-.-:::::::'     || ")
+    print("      /.'`;|:::::::'      ||_ ")
+    print("     ||   ||::::::'     _.;._'-._ ")
+    print("     ||   ||:::::'  _.-!oo @.!-._'-. ")
+    print("     ('.  ||:::::.-!()oo @!()@.-'_.| ")
+    print("      '.'-;|:.-'.&$@.& ()$%-'o.'-U|| ")
+    print("        `>'-.!@%()@'@_%-'_.-o _.|'|| ")
+    print("         ||-._'-.@.-'_.-' _.-o  |'|| ")
+    print("         ||=[ '-._.-+U/.-'    o |'|| ")
+    print("         || '-.]=|| |'|      o  |'|| ")
+    print("         ||      || |'|        _| '; ")
+    print("         ||      || |'|    _.-'_.-' ")
+    print("         |'-._   || |'|_.-'_.-' ")
+    print("          '-._'-.|| |' `_.-' ")
+    print("              '-.||_/.-' ")
+    print()
+
+def print_straznik():
+    print()
+    print("  ,   A           {}")
+    print(" / \, | ,        .--.")
+    print("|    =|= >      /.--.|")
+    print(" \ /` | `       |====|")
+    print("  `   |         |`::`|")
+    print("      |     .-;`\..../`;_.-^-._")
+    print("     /\\/  /  |...::..|`   :   `|")
+    print("     |:'\ |   /'''::''|   .:.   |")
+    print("      \ /\;-,/\   ::  |..:::::..|")
+    print("      |\ <` >  >._::_.| ':::::' |")
+    print("      | `""`  /   ^^  |   ':'   |")
+    print("      |       |       \    :    /")
+    print("      |       |        \   :   /")
+    print("      |       |___/\___|`-.:.-`")
+    print("      |        \_ || _/    `")
+    print("      |        <_ >< _>")
+    print("      |        |  ||  |")
+    print("      |        |  ||  |")
+    print("      |       _\.:||:./_")
+    print("      |      /____/\____|")
+    print()
+
+def print_koniec_gry():
+    print()
+    print("  K   K   OOOOO   N    N   I   EEEEE   CCCCC  ")
+    print("  K K     O   O   N N  N   I   E       C      ")
+    print("  K       O   O   N  N N   I   EEEEE   C      ")
+    print("  K K     O   O   N   NN   I   E       C      ")
+    print("  K  K    O   O   N   NN   I   E       C      ")
+    print("  K   K   OOOOO   N    N   I   EEEEE   CCCCC  ")
+    print()
+    print("     P   R   Z   E   G   R   A   N   A   ")
+    print()
+
+
+def smierc(dlaczego):
+    print_koniec_gry()
+    print("{}. Przykro mi...".format(dlaczego))
+    exit(0)
+
+
+def straznik():
+    print_straznik()
+
+    print("Podchodzisz obok strażnika, wciąż śpi...")
+    print("Trzask!...Przez swoją nieuwagę potrąciłeś leżące na ziemi metalowe wiadro")
+    print("\nEj ty, co ty tu robisz!?")
+
+    straznik_ruch = False
+
+    while True:
+        nastepna_czynnosc = input("[ucieczka | drzwi] > ").lower()
+        if nastepna_czynnosc == "ucieczka" and straznik_ruch:
+            smierc("Strażnik był szybszy niż ty, giniesz...")
+        elif nastepna_czynnosc == "ucieczka" and not straznik_ruch:
+            print("Strażnik jest strasznie zaspany, wciąż masz szanse na ucieczkę z powrotem lub poprzez drzwi za nim")
+            straznik_ruch = True
+        elif nastepna_czynnosc == "drzwi" and straznik_ruch:
+            print("Prześlizgujesz się przez drzwi zanim zauważa to strażnik")
+            print("Jesteś na zewnątrz, uwolniłeś się!")
+            return
+        elif nastepna_czynnosc == "drzwi" and not straznik_ruch:
+            smierc("Strażnik był szybszy niż ty, giniesz...")
+        else:
+            print("Spróbuj wpisać czynność ponownie [dzwi/ucieczka]")
+
+def komnata_skarbow():
+    print_skrzynia()
+    skrzynia_skarbow = ["klejnoty", "złoto", "srebro", "miecz"]
+    print("Widzisz komnatę z wielką zdobioną skrzynią po lewej i śpiącego strażnika po prawej, a za nim drzwi.")
+    
+    czynnosc = input("Co robisz? > ")
+
+    if czynnosc.lower() in ["skrzynię", "skrzynia", "lewo"]:
+        print("Skrzynia pełna skarbów?!") 
+
+        print("Czy chcesz ją otworzyć? Wpisz '1'")
+        print("Aby ją zostawić wpisz '2'")
+        wybor = input("> ")
+
+        if wybor == "1":
+            print("Zobaczmy co jest w środku")
+            print("Skrzynia skrzypi podczas otwierania, ale strażnik się nie budzi...")
+            print("Znajdujesz w niej")
+
+            for skarb in skrzynia_skarbow:
+                print(skarb)
+
+            print("Co robisz?")
+            print("Jeśli chcesz wziąć {}, wpisz '1'".format(len(skrzynia_skarbow)))
+            print("Jeśli nie chcesz nic brać, wpisz '2'")
+
+            wybor_skarbu = input("> ")
+            if wybor_skarbu == "1":
+                print("\tSuper! Piękny elficki miecz!")
+                print("\tOrzymałeś [{}]".format(", ".join(skrzynia_skarbow)))
+            elif wybor_skarbu == "2":
+                print("Ostroźność przede wszystkim...")
+        
+            straznik()
+    else:
+
+        print("Przyjrzyjmy się temu strażnikowi, ma chyba przy sobie sakiewkę pełną monet, idziemy w jego stronę")
+        straznik()
+
+
+def komnata_orka():
+    print_ork()
+    print("Ukazuje Ci się zmutowany ork")
+    print("Przygląda Ci się i zaczynasz wariować ze strachu")
+    print("Ucieczka czy pewna śmierć? Wybieraj")
+
+    nastepna_czynnosc = input("> ")
+
+    if "ucieczka" in nastepna_czynnosc:
+        poczatek()
+    else:
+
+        smierc("Ucieczka była jedynym dobrym wyjściem. Ork rzuca się na Ciebie z potwornym rykiem. Umierasz.")
+
+def komnata_szybkiej_smierci():
+    
+    print("Drzwi bardzo ciężko się otwierają...")
+    
+    czynnosc_komnata_szybkiej_smierci = input("Napierasz na nie dalej? > ")
+
+    if czynnosc_komnata_szybkiej_smierci.lower() in ["tak", "napieram"]:
+        print_zamek()
+        print("Ukazuje Ci się zamek, już myślisz że właśnie się uwolniłeś...")
+        print("...lecz za drzwiami czaiła się przepaść")
+        smierc("Spadasz w przepaść")
+    else:
+
+        print("Ostrożność przede wszystkim...")
+        poczatek()
+
+
+def imie_gracza():
+    imie = input("Jak masz na imię? > ")
+    print("Zaczynajmy przygodę " + imie)
+    return imie
+
+def poczatek():
+    print_loch()
+    print("Jesteś w pomieszczeniu przypominającym loch. Widzisz przed sobą 3 ciężkie drewniane drzwi ponumerowane od 1 do 3.")
+    wybor_drzwi = input("Które wybierasz, 1,2 czy 3? > ")
+
+    if wybor_drzwi == "1":
+        komnata_orka()
+    elif wybor_drzwi == "2":
+        komnata_skarbow()
+    elif wybor_drzwi == "3":
+        komnata_szybkiej_smierci()
+    else:
+        print("Wybór był prosty: 1,2 lub 3. Żegnaj..")
+
+def main():
+    gracz =  imie_gracza()
+
+    poczatek()
+    
+    print("\nKoniec\n")
+    print("Dzięki za grę, {}".format(gracz.upper()))
+    
+
+
+
+if __name__ == '__main__':
+    main()
